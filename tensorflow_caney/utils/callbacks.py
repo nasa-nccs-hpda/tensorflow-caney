2bcb659b41c146081bb04862fb38e533fe849ee1
Jordan A Caraballo-Vega <jordan.a.caraballo-vega@nasa.gov>
2022-06-17 15:06:13 -0400

    Documenting callbacks

--- tensorflow_caney/utils/callbacks.py
+++ tensorflow_caney/utils/callbacks.py
@@ -2,18 +2,21 @@ import sys
 import tensorflow as tf
 import segmentation_models as sm
 import tensorflow_addons as tfa
+from typing import List
 
-from typing import Any, List
 
-def get_callbacks(callbacks: List[str]) -> Any:
+def get_callbacks(callbacks: List[str]) -> List:
     """
     Get callback functions from string evaluation.
+    Args:
+        callbacks (List[str]): string with optimizer callable.
+    Returns:
+        List of Callables.
     """
     callback_functions = []
     for callback in callbacks:
         try:
             callback_functions.append(eval(callback))
         except NameError as err:
-            sys.exit(f'Handling run-time error: {err}')
+            sys.exit(f'{err}. Accepted models from {tf}, {sm}, {tfa}')
     return callback_functions
-
