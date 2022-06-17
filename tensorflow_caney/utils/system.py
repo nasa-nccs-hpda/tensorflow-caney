6b96854a2df7cc0ca039ebd081d4473d41912c5c
Jordan A Caraballo-Vega <jordan.a.caraballo-vega@nasa.gov>
2022-06-17 14:19:34 -0400

    Modifications to system with documentation

--- tensorflow_caney/utils/system.py
+++ tensorflow_caney/utils/system.py
@@ -1,23 +1,36 @@
 import os
 import numpy as np
-import cupy as cp
 import tensorflow as tf
 from tensorflow.python.distribute.mirrored_strategy import MirroredStrategy
 
+try:
+    import cupy as cp
+    HAS_GPU = True
+except ImportError:
+    HAS_GPU = False
 
 def seed_everything(seed: int = 42) -> None:
     """
-    Seed libraries.
+    Seeds starting randomization from libraries.
+    Args:
+        seed (int): integer to seed libraries with.
+    Returns:
+        None.
     """
     np.random.seed(seed)
     tf.random.set_seed(seed)
-    cp.random.seed(seed)
+    if HAS_GPU:
+        cp.random.seed(seed)
     return
 
 
 def set_gpu_strategy(gpu_devices: str = "0,1,2,3") -> MirroredStrategy:
     """
     Set training strategy.
+    Args:
+        gpu_devices (str): string with gpu devices separated by commas.
+    Returns:
+        tf.MirroredStrategy() for multi-gpu training.
     """
     os.environ['CUDA_VISIBLE_DEVICES'] = gpu_devices
     devices = tf.config.list_physical_devices('GPU')
@@ -28,6 +41,10 @@ def set_gpu_strategy(gpu_devices: str = "0,1,2,3") -> MirroredStrategy:
 def set_mixed_precision(mixed_precision: bool = True) -> None:
     """
     Enable mixed precision.
+    Args:
+        mixed_precision (bool): enable mixed precision.
+    Returns:
+        None.
     """
     if mixed_precision:
         policy = tf.keras.mixed_precision.Policy('mixed_float16')
@@ -37,7 +54,11 @@ def set_mixed_precision(mixed_precision: bool = True) -> None:
 
 def set_xla(xla: bool = False) -> None:
     """
-    Enable linear acceleration.
+    Enable linear algebra acceleration.
+    Args:
+        xla (bool): enable xla.
+    Returns:
+        None.
     """
     if xla:
         tf.config.optimizer.set_jit(True)
