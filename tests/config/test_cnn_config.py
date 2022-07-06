7ecd2b1d0e4baa5569f29710c76ed15a53bd0c60
Jordan A Caraballo-Vega <jordan.a.caraballo-vega@nasa.gov>
2022-07-05 22:41:44 -0400

    Adding config test

--- tests/config/test_cnn_config.py
+++ tests/config/test_cnn_config.py
@@ -1,13 +1,29 @@
-# import pytest
+import sys
+import pytest
+from omegaconf import OmegaConf
+from tensorflow_caney.config.cnn_config import Config
 
-"""
-def test_config_yaml():
 
+@pytest.mark.parametrize(
+    "filename, expected_experiment_name, expected_experiment_type",
+    [(
+        'tests/data/land_cover_test.yaml',
+        'landcover-trees',
+        'landcover'
+    )]
+)
+def test_config_yaml(
+            filename: str,
+            expected_experiment_name: str,
+            expected_experiment_type: str
+        ):
     schema = OmegaConf.structured(Config)
-    conf = OmegaConf.load("../config/config_clouds/vietnam_clouds.yaml")
+    conf = OmegaConf.load(filename)
+
     try:
         conf = OmegaConf.merge(schema, conf)
     except BaseException as err:
         sys.exit(f"ERROR: {err}")
-    sys.exit(conf)
-"""
+
+    assert expected_experiment_name == conf.experiment_name
+    assert expected_experiment_type == conf.experiment_type
