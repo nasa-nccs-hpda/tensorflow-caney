afea5bf6891456fe114003ae10fb05cd0e01654d
jordancaraballo <jordan.a.caraballo-vega@nasa.gov>
2023-03-01 10:00:05 -0500

    Adding changes to support PEP8

--- tensorflow_caney/model/vegetation_indices.py
+++ tensorflow_caney/model/vegetation_indices.py
@@ -14,13 +14,11 @@ class VegetationIndices(GeospatialFile):
         print("Calculate ndvi here")
 
 
-
 if __name__ == "__main__":
 
     raster = VegetationIndices(
-        '/explore/nobackup/projects/ilab/data/AGB/Airborne_Hyperspectral/DP3.30006.001/neon-aop-products/2013/FullSite/D17/2013_SJER_1/L3/Spectrometer/Reflectance/2013_NEON_D17_SJER_DP3_257000_4105000_reflectance.h5',
+        '2013_NEON_D17_SJER_DP3_257000_4105000_reflectance.h5',
         ['ndvi']
     )
     print(len(raster.data))
     print(raster.indices_list)
-
