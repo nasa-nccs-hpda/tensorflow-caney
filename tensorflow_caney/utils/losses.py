00ecf9e3b01950206cc3df54a301b7588a05da44
jordancaraballo <jordan.a.caraballo-vega@nasa.gov>
2023-12-20 19:09:34 -0500

    Completing PEP8 compliance

--- tensorflow_caney/utils/losses.py
+++ tensorflow_caney/utils/losses.py
@@ -368,8 +368,8 @@ def tversky(y_true, y_pred, alpha=0.40, beta=0.60):
 def class_tversky(y_true, y_pred):
     smooth = 1
 
-    y_true = K.permute_dimensions(y_true, (3,1,2,0))
-    y_pred = K.permute_dimensions(y_pred, (3,1,2,0))
+    y_true = K.permute_dimensions(y_true, (3, 1, 2, 0))
+    y_pred = K.permute_dimensions(y_pred, (3, 1, 2, 0))
 
     y_true_pos = K.batch_flatten(y_true)
     y_pred_pos = K.batch_flatten(y_pred)
@@ -377,10 +377,11 @@ def class_tversky(y_true, y_pred):
     false_neg = K.sum(y_true_pos * (1-y_pred_pos), 1)
     false_pos = K.sum((1-y_true_pos)*y_pred_pos, 1)
     alpha = 0.7
-    return (true_pos + smooth)/(true_pos + alpha*false_neg + (1-alpha)*false_pos + smooth)
+    return (true_pos + smooth) / \
+        (true_pos + alpha * false_neg + (1 - alpha) * false_pos + smooth)
 
 
-def focal_tversky_loss(y_true,y_pred):
+def focal_tversky_loss(y_true, y_pred):
     pt_1 = class_tversky(y_true, y_pred)
     gamma = 0.75
     return K.sum(K.pow((1-pt_1), gamma))
@@ -393,7 +394,8 @@ def binary_tversky(y_true, y_pred):
     false_neg = K.sum(y_true_pos * (1-y_pred_pos))
     false_pos = K.sum((1-y_true_pos)*y_pred_pos)
     alpha = 0.7
-    return (true_pos + smooth)/(true_pos + alpha*false_neg + (1-alpha)*false_pos + smooth)
+    return (true_pos + smooth) / \
+        (true_pos + alpha * false_neg + (1 - alpha) * false_pos + smooth)
 
 
 def binary_tversky_loss(y_true, y_pred):
