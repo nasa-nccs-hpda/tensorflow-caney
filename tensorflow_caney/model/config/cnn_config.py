be06e4c83b890ca7c7cd0b31b761500016d08be4
jordancaraballo <jordan.a.caraballo-vega@nasa.gov>
2024-02-12 15:06:43 -0500

    Changing padding value, fixes #40 as well

--- tensorflow_caney/model/config/cnn_config.py
+++ tensorflow_caney/model/config/cnn_config.py
@@ -95,6 +95,9 @@ class Config:
     # Specify standardization mechanism
     standardization: Optional[str] = None
 
+    # Input nodata (only used if raster does not have no-data defined)
+    input_nodata: Optional[float] = -10001
+
     # Specify CNN batch size for training
     batch_size: int = 32
 
@@ -163,7 +166,7 @@ class Config:
     inference_treshold: Optional[float] = 0.5
 
     # Inference padding value to replace no-data boundaries
-    inference_pad_value: Optional[int] = 1000
+    inference_pad_value: Optional[float] = 1000
 
     # Window algorithm for smoothing prediction
     window_algorithm: Optional[str] = 'triang'
